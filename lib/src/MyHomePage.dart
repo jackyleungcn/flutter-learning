f0b936c7e812043fb752d8599d8829c5c99575cd
liangjunqi <liangjunqi@youcheyihou.com>
2019-08-05 17:36:02 +0800

    step<Flutter实战>: 基础Weigets -> CounterWidget

--- lib/src/MyHomePage.dart
+++ lib/src/MyHomePage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './RandomWordsWidget.dart';
+import './CounterWidget.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -10,50 +10,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text(widget.title),
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            FlatButton(
-                child: Text("open new route"),
-                textColor: Colors.blue,
-                onPressed: () {
-                  Navigator.of(context).pushNamed("new_page", arguments: "hi");
-                }),
-            RandomWordsWidget(),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: CounterWidget(),
     );
   }
 }
-
-
-
-
