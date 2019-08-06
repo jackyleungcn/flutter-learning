af0e4239f8bc83364e44777b15dd2742ade5e21b
liangjunqi <liangjunqi@youcheyihou.com>
2019-08-06 10:39:47 +0800

    step<Flutter实战>: 基础Widgets -> 混合管理

---
+++ lib/src/TapboxC.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class TapboxC extends StatefulWidget {
+  TapboxC({
+    Key key,
+    this.active: false,
+    @required this.onChanged,
+  }) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapboxCState createState() => new _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            widget.active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+            border: _highlight
+                ? new Border.all(
+                    color: Colors.teal[700],
+                    width: 10.0,
+                  )
+                : null),
+      ),
+    );
+  }
+}
---
+++ lib/src/parentWidgetC.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import './TapboxC.dart';
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
